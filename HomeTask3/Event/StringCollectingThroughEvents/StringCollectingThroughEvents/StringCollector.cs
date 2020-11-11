1ac1e74baf1297014b9381989acaeddf757e85b6
ILYABELYAEV2A78\ilyabelyaev <belyaev.i2000@outlook.com>
2020-11-11 03:00:46 +0200

    Event version add

---
+++ HomeTask3/Event/StringCollectingThroughEvents/StringCollectingThroughEvents/StringCollector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringCollectingThroughEvents
+{
+    class StringCollector
+    {
+        private List<string> strings = new List<string>();
+
+        public void Add(string inputString)
+        {
+            strings.Add(inputString);
+        }
+
+        public void Print()
+        {
+            foreach (var str in strings)
+            {
+                Console.WriteLine($"{str}");
+            }
+        }
+    }
+}
