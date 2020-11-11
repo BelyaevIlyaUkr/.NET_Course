b62df6fe0a7652d96aa67da4f208f3306ddf5fe3
ILYABELYAEV2A78\ilyabelyaev <belyaev.i2000@outlook.com>
2020-11-11 04:09:04 +0200

    Delegate version add

---
+++ HomeTask3/Delegate/StringCollectingThroughDelegates/StringCollectingThroughDelegates/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace StringCollectingThroughDelegates
+{
+    public delegate void AlphaNumbericCollectorPublisher(string inputString);
+
+    public delegate void StringCollectorPublisher(string inputString);
+
+    class Program
+    {
+        public static AlphaNumbericCollectorPublisher alphaNumbericCollectorPublisher;
+
+        public static StringCollectorPublisher stringCollectorPublisher;
+
+        static void Main(string[] args)
+        {
+            AlphaNumbericCollector alphaNumbericCollector = new AlphaNumbericCollector();
+            StringCollector stringCollector = new StringCollector();
+
+            alphaNumbericCollectorPublisher = alphaNumbericCollector.Add;
+            stringCollectorPublisher = stringCollector.Add;
+
+            while (true)
+            {
+                Console.WriteLine("Enter string");
+
+                string inputString = Console.ReadLine();
+
+                bool isNumberContaining = false;
+
+                foreach (char ch in inputString)
+                {
+                    if (char.IsDigit(ch))
+                    {
+                        isNumberContaining = true;
+                        break;
+                    }
+                }
+
+                if (isNumberContaining)
+                    alphaNumbericCollectorPublisher.Invoke(inputString);
+                else
+                    stringCollectorPublisher.Invoke(inputString);
+
+                Console.WriteLine("AlphaNumbericCollector:");
+                alphaNumbericCollector.Print();
+
+                Console.WriteLine("StringCollector:");
+                stringCollector.Print();
+
+                while (true)
+                {
+                    Console.WriteLine("Continue?(Y/N)");
+                    var continuation = Console.ReadKey();
+                    Console.Write("\n");
+                    if (continuation.KeyChar == 'N')
+                        return;
+                    else if (continuation.KeyChar == 'Y')
+                        break;
+                    Console.WriteLine("Invalid character. Enter again.");
+                }
+            }
+        }
+    }
+}
---
+++ HomeTask3/Delegate/StringCollectingThroughDelegates/StringCollectingThroughDelegates/StringCollector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringCollectingThroughDelegates
+{
+    class StringCollector
+    {
+        private List<string> strings = new List<string>();
+
+        public void Add(string inputString)
+        {
+            strings.Add(inputString);
+        }
+
+        public void Print()
+        {
+            foreach (var str in strings)
+            {
+                Console.WriteLine($"{str}");
+            }
+        }
+    }
+}
