19883f449dc1856a5115a4479ba0fe45e5294974
ILYABELYAEV2A78\ilyabelyaev <belyaev.i2000@outlook.com>
2020-11-12 23:46:31 +0200

    Small corrections in common songs length task

--- HomeTask4/Songs_Length/Songs_Length/Program.cs
+++ HomeTask4/Songs_Length/Songs_Length/Program.cs
@@ -9,8 +9,10 @@ namespace Songs_Length
         {
             string inputString = "4:12,2:43,3:51,4:29,3:24,3:14,4:46,3:25,4:52,3:27";
 
-            var songsCollection = inputString.Split(',')
-                .Select(song => song.Split(':')).Select(splittedSong => new { minutes = splittedSong[0], seconds = splittedSong[1]});
+            var songsCollection = inputString
+                .Split(',')
+                .Select(song => song.Split(':'))
+                .Select(splittedSong => new { minutes = splittedSong[0], seconds = splittedSong[1]});
 
             int commonSongsLengthInSeconds = songsCollection.Aggregate(0, (commonLength,song) 
                 => commonLength + Convert.ToInt32(song.minutes) * 60 + Convert.ToInt32(song.seconds));
