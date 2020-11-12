0b946e9d62f3042938cf5b52b440865a069e7bfc
ILYABELYAEV2A78\ilyabelyaev <belyaev.i2000@outlook.com>
2020-11-12 17:51:33 +0200

    Hometask4 - Gamers

---
+++ HomeTask4/Gamers/Gamers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Gamers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = "Davis, Clyne, Fonte, Hooiveld, Shaw, Davis, Schneiderlin, Cork, Lallana, Rodriguez, Lambert";
+
+            string[] splittedString = inputString.Split(", ");
+            
+            var changedSplittedString = Enumerable.Range(1, splittedString.Length).Zip(splittedString, (first, second) => first + ". " + second);
+            
+            string resultString = string.Join(", ", changedSplittedString);
+            
+            Console.WriteLine(resultString);
+
+            Console.ReadKey();
+        }
+    }
+}
