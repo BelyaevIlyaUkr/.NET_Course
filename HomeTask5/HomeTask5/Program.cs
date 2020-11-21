d14905837dec16d0e3d070206b90afba690a32f0
ILYABELYAEV2A78\ilyabelyaev <belyaev.i2000@outlook.com>
2020-11-21 05:19:07 +0200

    Initial commit

---
+++ HomeTask5/HomeTask5/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace HomeTask5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string sCreateDatabase = "CREATE DATABASE StudyManager";
+
+            /*string sCreateTable = "CREATE TABLE Grades(GradeID INTEGER PRIMARY KEY IDENTITY," +
+            "GradeDate DATE NOT NULL, IsComplete BIT NOT NULL," +
+            "HomeTaskID INT NOT NULL FOREIGN KEY REFERENCES HomeTasks(HomeTaskID) ON DELETE CASCADE," +
+            "StudentID INT NOT NULL FOREIGN KEY REFERENCES Students(StudentID) ON DELETE CASCADE)";*/
+
+            /*string sCreateTable = "CREATE TABLE HomeTasks(HomeTaskID INTEGER PRIMARY KEY IDENTITY, Name VARCHAR(80) NOT NULL," +
+            "Description TEXT,TaskDate DATE NOT NULL,SerialNumber INT NOT NULL,CourseID INT NOT NULL FOREIGN KEY REFERENCES Courses(CourseID) ON DELETE CASCADE)";*/
+
+            /*string sCreateTable = "CREATE TABLE Students_Courses (StudentID INT NOT NULL FOREIGN KEY " +
+            "REFERENCES Students(StudentID) ON DELETE CASCADE," +
+            "CourseID INT NOT NULL FOREIGN KEY REFERENCES Courses(CourseID) ON DELETE CASCADE," +
+            "PRIMARY KEY (StudentID,CourseID), UNIQUE(StudentID,CourseID))";*/
+
+            string sCreateTable = "CREATE TABLE Courses_Lecturers (CourseID INT NOT NULL FOREIGN KEY " +
+            "REFERENCES Courses(CourseID) ON DELETE CASCADE," +
+            "LecturerID INT NOT NULL FOREIGN KEY REFERENCES Lecturers(LecturerID) ON DELETE CASCADE," +
+            "PRIMARY KEY (CourseID,LecturerID), UNIQUE(CourseID,LecturerID))";
+
+
+            SqlConnection mycon = new SqlConnection();
+            mycon.ConnectionString = "Data Source=ILYABELYAEV2A78; Initial Catalog = StudyManager; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+
+            SqlCommand mycomm = new SqlCommand();
+            mycomm.CommandType = CommandType.Text;
+            mycomm.Connection = mycon;
+            mycomm.CommandText = sCreateTable;
+
+            try
+            {
+                mycon.Open();
+                mycomm.ExecuteNonQuery();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                mycon.Close();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
