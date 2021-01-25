f38faead555df1feffa196f4012ad097c760c8ca
ILYABELYAEV7B3F\ilyabelyaev <belyaev.i2000@outlook.com>
2021-01-25 10:44:08 +0200

    Hometask 10 initial commit

---
+++ HomeTask10/HomeTask10/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HomeTask10.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
